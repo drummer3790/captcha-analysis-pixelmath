41d380dd8a9890dfa6132e28268505c4ca7f49c8
snidemic <snidemic@4e8ce266-54a6-4726-9732-1309dff98d05>
2011-12-07 12:33:33 +0000

    Vastly improved functionality by creating the NeuralNetwork class which holds all of the layers of the network and it's various important values.

--- trunk/outputNode.py
+++ trunk/outputNode.py
@@ -4,9 +4,7 @@ class outputNode:
 	def __init__(self, num_nodes, letterOffset):
 		self.weights = [random() for i in inputs]
         self.letter = chr(ord('A') + letterOffset)
-		self.inputs = None
-		self.output = None
-	
+
 	def __str__(self):
 		return str(self.letter)
 		
@@ -24,10 +22,3 @@ class outputNode:
 		
 	def adjustWeight(input, error):
 		self.weights[input] -= error 
-
-	def output():
-        return sum([self.inputs[i]*self.weights[i] for i in self.inputs)
-	
-	def setInputs(inputs):
-		self.inputs = inputs
-		self.output = output()
