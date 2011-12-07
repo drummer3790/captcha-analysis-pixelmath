61bee0b74a5f226dce75f934804125c04e81d806
snidemic <snidemic@4e8ce266-54a6-4726-9732-1309dff98d05>
2011-12-07 06:08:45 +0000

    Modified the outputNode class and finished the forward propagation.

--- trunk/hiddenNode.py
+++ trunk/hiddenNode.py
@@ -3,13 +3,13 @@ from random import random
 
 class hiddenNode:
 	def __init__(self, num_nodes):
-		self.weightsIn = [random() for i in num_nodes]
+		self.weights = [random() for i in num_nodes]
 		
 	def adjustWeight(input, error):
 		self.weights[input] -= error 
 
 	def output():
-                return sigmoid_func(sum([self.inputs[i]*self.weights[i] for i in self.weights]))
+        return sigmoid_func(sum([self.inputs[i]*self.weights[i] for i in self.weights]))
 	
 	def sigmoid_func(h):
 		return 1/(1+math.exp(-h))
--- trunk/outputNode.py
+++ trunk/outputNode.py
@@ -3,13 +3,31 @@ from random import random
 class outputNode:
 	def __init__(self, num_nodes, letterOffset):
 		self.weights = [random() for i in inputs]
-        self.value = chr(ord('A') + letterOffset)
+        self.letter = chr(ord('A') + letterOffset)
+		self.inputs = None
+		self.output = None
+	
+	def __str__(self):
+		return str(self.letter)
+		
+	def __lt__(self, other):
+		return self.output < other.output
+	
+	def __gt__(self, other):
+		return self.output > other.output
+	
+	def __eq__(self, other):
+		return self.output == other.output
+	
+	def __ne__(self, other):
+		return self.output != other.output
 		
 	def adjustWeight(input, error):
 		self.weights[input] -= error 
 
 	def output():
-                return sum([self.inputs[i]*self.weights[i] for i in self.inputs)
+        return sum([self.inputs[i]*self.weights[i] for i in self.inputs)
 	
 	def setInputs(inputs):
 		self.inputs = inputs
+		self.output = output()
